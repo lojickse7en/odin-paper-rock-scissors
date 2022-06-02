645b3f8b7768c43f068a58cdbd75505dc0a01a6b
Bayo <acadewole@gmail.com>
2022-06-02 15:09:49 -0500

    Clean up JS code

--- javascript.js
+++ javascript.js
@@ -1,54 +1,3 @@
-// for (let i=1; i<=3; i++) {
-//     const prcOptions = ['paper', 'rock', 'scissors']
-//     const playerSelection = (prompt('Paper, Rock or Scissors?')).toLowerCase();
-//     const computerSelection = prcOptions[Math.floor(Math.random() * prcOptions.length)];
-// }
-//     let playRound = (playerSelection, computerSelection) => {
-
-//         if (playerSelection === 'rock' && computerSelection === 'rock' ) {
-//             return result = 'Its a tie'
-//         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-//             return result = 'You lose😭! Paper beats rock'
-//         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-//             return result = 'You win! Rock beats scissors'
-//         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-//             return result = 'You win! Paper beats rock'
-//         } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-//             return result = 'Its a tie'
-//         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-//             return result = 'You lose😭! Scissors beats paper'
-//         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-//             return result = 'You lose😭! Rock beats scissors'
-//         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-//             return result = 'You win! Scissors beats paper'
-//         } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-//             return result = 'Its a tie'
-//         } else return alert('You can only type, paper, rock or scissors 😕')
-        
-//     }
-  
-    
-
-
-
-
-// let game = () => {
-    
-//     let playerScore = 0; 
-//     let computerScore = 0;
-
-//      if (playRound() == ('You win! Scissors beats paper)' || ('You win! Paper beats rock') || ('You win! Rock beats scissors'))) {
-//        return console.log(playerScore++);
-//     }
-//     else if (playRound() == 'You lose😭! Rock beats scissors' || 'You lose😭! Scissors beats paper' || 'You lose😭! Paper beats rock') {
-//         return console.log(computerScore++);
-//     }
-   
-// }
-
-// console.log(game());
-// console.log(playerScore, computerScore);
-
 //loops through the code block 'i' times
 for (let i=1; i<=3; i++) {
 //function that generates a random (paper, rock or scissors) response for the computer. Returns the value
@@ -92,6 +41,7 @@ let playRound = (playerSelection, computerSelection) => {
 
 roundResult = playRound(playerSelection.toLowerCase(), computerSelection);
 
+
 let playerScore = 0; 
 let computerScore = 0;
 
@@ -112,50 +62,3 @@ let gameScore = (roundResult) => {
 gameScore()
 console.log(computerScore, playerScore)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let game = () => {
-//     //we want the user to be prompted with an option to enter RPS. The results of the selection will record a score. 
-//     let playerScore = 0; 
-//     let computerScore = 0;
-
-//     for (let i=1; i<=3; i++) {
-//         const playerSelection = prompt('Paper, Rock or Scissors?');
-//         const computerSelection = computerPlay();
-//         roundResult = playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
-
-//         console.log(roundResult);
-//     } if (roundResult == 'You win! Scissors beats paper' || 'You win! Paper beats rock' || 'You win! Rock beats scissors') {
-//         score++;
-//     }
-//     else if (roundResult == 'You lose😭! Rock beats scissors' || 'You lose😭! Scissors beats paper' || 'You lose😭! Paper beats rock') {
-//         score--;
-//     }
-//     else {
-//         score = score;
-//     }
-//         return console.log(score);
-    
-// }
-
-
-
-// console.log(game());
